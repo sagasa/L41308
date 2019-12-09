57734289bf1d4cf64109db92ae4a080a1c09751c
Haru0213 <loveliveabcdzz@gmail.com>
2019-12-09 16:25:33 +0900

    Map作成中

---
+++ Map/Camera.cs
@@ -0,0 +1,54 @@
+﻿using DxLibDLL;
+
+namespace Giraffe
+{
+    // 画面のスクロール量を管理するクラス。
+    // 画像の描画機能を持つ。
+    // スクロールの影響を受けるオブジェクトはこのクラスを通じて描画することで
+    // スクロールを意識せずに描画を行うことができる。
+    public static class Camera
+    {
+        // カメラの位置。
+        // 画面左上のワールド座標を表す。
+        public static float x;
+        public static float y;
+
+        // 指定されたワールド座標が画面の中心に来るように、カメラの位置を変更する
+        public static void LookAt(float targetX)
+        {
+            x = targetX - Screen.Width / 2;
+        }
+
+        /// <summary>
+        /// 画像を描画する。スクロールの影響を受ける。
+        /// </summary>
+        /// <param name="worldX">左端のx座標</param>
+        /// <param name="worldY">上端のy座標</param>
+        /// <param name="handle">画像ハンドル</param>
+        /// <param name="flip">左右反転するならtrue, しないならfalse（反転しない場合は省略可）</param>
+        public static void DrawGraph(float worldX, float worldY, int handle, bool flip = false)
+        {
+            if (flip) DX.DrawTurnGraphF(worldX - x, worldY - y, handle);
+            else DX.DrawGraphF(worldX - x, worldY - y, handle);
+        }
+
+        /// <summary>
+        /// 四角形（枠線のみ）を描画する
+        /// </summary>
+        /// <param name="left">左端</param>
+        /// <param name="top">上端</param>
+        /// <param name="right">右端</param>
+        /// <param name="bottom">下端</param>
+        /// <param name="color">色</param>
+        public static void DrawLineBox(float left, float top, float right, float bottom, uint color)
+        {
+            DX.DrawBox(
+                (int)(left - x + 0.5f),
+                (int)(top - y + 0.5f),
+                (int)(right - x + 0.5f),
+                (int)(bottom - y + 0.5f),
+                color,
+                DX.FALSE);
+        }
+    }
+}
--- Map/PlayMap.cs
+++ Map/PlayMap.cs
@@ -1,14 +1,139 @@
-﻿namespace Giraffe
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using DxLibDLL;
+
+namespace Giraffe
 {
     public class PlayMap
     {
-        private int[,] mapData;
+        public const int None = -1;
+
+        const int Width = 10;
+        const int Height = 25;
+        const int CellSize = 64;
+
+        ScenePlay scenePlay;
+        int[,] mapData;
+
+        public PlayMap(ScenePlay scenePlay, string filePath)
+        {
+            this.scenePlay = scenePlay;
+            Load("Map/" + filePath + ".csv");
+            LoadObject("Map/" + filePath + ".csv");
+        }
+
+        void Load(string filePath)
+        {
+            mapData = new int[Width, Height];
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            Debug.Assert(lines.Length == Height, filePath + "CSVファイルの高さが不正です:" + lines.Length);
+
+            for (int y = 0; y < Height; y++)
+            {
+                string[] splitted = lines[y].Split(new char[] { ',' });
+
+                Debug.Assert(splitted.Length == Width, "CSVファイルの" + y + "行目の列数が不正です:" +
+                    splitted.Length);
+
+                for (int x = 0; x < Width; x++)
+                {
+                    mapData[x, y] = int.Parse(splitted[x]);
+                }
+            }
+        }
+
+        void LoadObject(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            Debug.Assert(lines.Length == Height, filePath + "の高さが不正です:" + lines.Length);
+
+            for (int y = 0; y < Height; y++)
+            {
+                string[] splitted = lines[y].Split(new char[] { ',' });
+
+                Debug.Assert(splitted.Length == Width, filePath + "の" + y + "行目の列数が不正です:" + splitted.Length);
+
+                for (int x = 0; x < Width; x++)
+                {
+                    int id = int.Parse(splitted[x]);
+
+                    if (id == -1) continue;
+
+                    SpawnObject(x, y, id);
+                }
+            }
+        }
+
+        void SpawnObject(int mapX, int mapY, int objectID)
+        {
+            // 生成位置
+            float spawnX = mapX * CellSize;
+            float spawnY = mapY * CellSize;
+
+            if (objectID == 0) //Leaf
+            {
+               
+                scenePlay.gameObjects.Add( new Leaf(scenePlay,spawnX,spawnY));
+            }
+            else
+            {
+                Debug.Assert(false, "オブジェクトID" + objectID + "番の生成処理は未実装です。");
+            }
+        }
+
+        //public void DrawTerrain()
+        //{
+        //    // 画面内のマップのみ描画するようにする 
+        //    int left = (int)(Camera.x / CellSize);
+        //    int top = (int)(Camera.y / CellSize);
+        //    int right = (int)((Camera.x + Screen.Width - 1) / CellSize);
+        //    int bottom = (int)((Camera.y + Screen.Height - 1) / CellSize);
+
+        //    if (left < 0) left = 0;
+        //    if (top < 0) top = 0;
+        //    if (right >= Width) right = Width - 1;
+        //    if (bottom >= Height) bottom = Height - 1;
+
+        //    for (int y = top; y <= bottom; y++)
+        //    {
+        //        for (int x = left; x <= right; x++)
+        //        {
+        //            int id = mapData[x, y];
+
+        //            if (id == None) continue; // 描画しない 
+
+        //            Camera.DrawGraph(x * CellSize, y * CellSize, Image.mapchip[id]);
+        //        }
+        //    }
+        //}
+
+        //public int GetTerrain(float worldX, float worldY)
+        //{
+        //    // マップ座標系（二次元配列の行と列）に変換する 
+        //    int mapX = (int)(worldX / CellSize);
+        //    int mapY = (int)(worldY / CellSize);
+
+        //    // 二次元配列の範囲外は、何もないものとして扱う 
+        //    if (mapX < 0 || mapX >= Width || mapY < 0 || mapY >= Height)
+        //        return None;
+
+        //    return mapData[mapX, mapY]; // 二次元配列から地形IDを取り出して返却する
+        //}
+
         public void Draw()
         {
            
-
-           
-        
         }
     }
 }
+
+
+
