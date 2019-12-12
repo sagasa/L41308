5260d991d7e55bad22ca6ab02df566998304be50
kumagai <kumagaitakuma75@gmail.com>
2019-12-12 12:04:41 +0900

    BGMの移動の機能の追加

--- Sound/SoundManager.cs
+++ Sound/SoundManager.cs
@@ -13,27 +13,32 @@ namespace Giraffe
         public static Dictionary<string, DX.VECTOR> bgmPos = new Dictionary<string, DX.VECTOR>();
 
         static float bgmDis = 100.0f;//聞こえる範囲
-        static float speed = 1.0f / 6.0f;//リスナーの移動速度
-
         static float interval = 150.0f;
+
+        static float fadeSpeed = bgmDis / 60.0f;
+        static float crossSpeed = interval / 60.0f;
 
         public static bool fadeInit = false;
         public static void Load()
         {
-            ListenerPos = DX.VGet(0.0f, 0.0f, 0.0f);//リスナーの位置
-            ListenerDir = DX.VGet(0.0f, 0.0f, 1.0f);//リスナーの向き
+            ListenerPos = DX.VGet(0.0f, 1.0f, 0.0f);//リスナーの位置
+            ListenerDir = DX.VGet(0.0f, -1.0f, 0.0f);//リスナーの向き
             DX.Set3DSoundListenerPosAndFrontPos_UpVecY(ListenerPos, ListenerDir);//向きと位置を設定
 
             bgmMap["title"] = 0;
             bgmMap["play"] = 0;
             bgmMap["result"] = 0;
+            bgmMap["play_fast"] = 0;
+            bgmMap["tutorial"] = 0;
 
             bgmPos["title"] = DX.VGet(100.0f, 0.0f, 0.0f);//BGMの初期位置の設定
             bgmPos["play"] = DX.VGet(250.0f, 0.0f, 0.0f);
             bgmPos["result"] = DX.VGet(400.0f, 0.0f, 0.0f);
+            bgmPos["tutorial"] = DX.VGet(550.0f, 0.0f, 0.0f);
+            bgmPos["play_fast"] = bgmPos["play"];
         }
 
-        public static void FadeIn(string name)
+        public static void FadeIn(string name, int time)
         {
             if (!fadeInit)//初期化
             {
@@ -44,7 +49,7 @@ namespace Giraffe
 
             if (ListenerPos.z > bgmPos[name].z)
             {
-                ListenerPos.z -= speed;
+                ListenerPos.z -= fadeSpeed / time;
                 DX.Set3DSoundListenerPosAndFrontPos_UpVecY(ListenerPos, ListenerDir);
             }
             else if (ListenerPos.z < bgmPos[name].z)
@@ -56,7 +61,7 @@ namespace Giraffe
             PlayBgm(name);
         }
 
-        public static void FadeOut(string name)
+        public static void FadeOut(string name, int time)
         {
             if (!fadeInit)//初期化
             {
@@ -67,7 +72,7 @@ namespace Giraffe
 
             if (ListenerPos.z < bgmPos[name].z + bgmDis + 10)
             {
-                ListenerPos.z += speed;
+                ListenerPos.z += fadeSpeed / time;
                 DX.Set3DSoundListenerPosAndFrontPos_UpVecY(ListenerPos, ListenerDir);
             }
             else if (ListenerPos.z > bgmPos[name].z + bgmDis + 10)
@@ -79,7 +84,7 @@ namespace Giraffe
             PlayBgm(name);
         }
 
-        public static void CrossFade(string name1, string name2)
+        public static void CrossFade(string name1, string name2, int time)
         {
             if (!fadeInit)
             {
@@ -91,7 +96,7 @@ namespace Giraffe
 
             if (ListenerPos.x < bgmPos[name2].x)
             {
-                ListenerPos.x += speed;
+                ListenerPos.x += crossSpeed / time;
                 DX.Set3DSoundListenerPosAndFrontPos_UpVecY(ListenerPos, ListenerDir);
             }
             else if (ListenerPos.x > bgmPos[name2].x)
@@ -103,13 +108,20 @@ namespace Giraffe
             PlayBgm(name2);
         }
 
-        public static void ListenerMove()//テスト用、使い終わったら消去します
+        public static void BgmMove(string name)
         {
-            ListenerPos.x += speed;
+            bgmPos[name] = DX.VGet(bgmPos[name].x - fadeSpeed, bgmPos[name].y, bgmPos[name].z);
+            DX.Set3DPositionSoundMem(bgmPos[name], bgmMap[name]);
+            PlayBgm(name);
+        }
+
+        public static void ListenerMove(int time)//テスト用、使い終わったら消去します
+        {
+            ListenerPos.x += fadeSpeed / time;
             DX.Set3DSoundListenerPosAndFrontPos_UpVecY(ListenerPos, ListenerDir);
         }
 
-        public static void PlayBgm(string name)
+        public static void PlayBgm(string name)//テストで使うのでpublic、使い終わったらpublicは消す
         {
             //BGMの当たり判定、斜め移動してないのでとりあえず四角形で計算
             if (DX.CheckSoundMem(bgmMap[name]) != 1 &&
