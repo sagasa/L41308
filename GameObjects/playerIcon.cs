2be3fb7d1e44314b808933713cc201dde49dd982
bando <magnificent.immortal.sage@ezweb.ne.jp>
2019-12-16 02:41:22 +0900

    プレイヤーアイコンcsの修正

--- GameObjects/playerIcon.cs
+++ GameObjects/playerIcon.cs
@@ -10,7 +10,7 @@ namespace Giraffe
 {
     public class playerIcon:GameObject
     {
-        int a;
+        int iconpos;
         private int image = ResourceLoader.GetGraph("キリンアイコン.png");
         
         public playerIcon(Scene scene) : base(scene)
@@ -21,16 +21,33 @@ namespace Giraffe
 
         public override void Draw()
         {
-            DX.DrawGraphF(520, Y+a, image);
+            DX.DrawGraphF(520, Y+iconpos, image);
 
         }
         public override void Update()
         {
-            a = 460;
-           
-             if (Input.UP.IsHold())
+            iconpos = 460;
+
+            if (Input.RIGHT.IsHold())
             {
-                pos = pos + new Vec2f(0, -1f);
+                pos = pos + new Vec2f(0, -0.5f);
+            }
+            else if (Input.LEFT.IsHold())
+            {
+                pos = pos + new Vec2f(0, -0.5f);
+            }
+
+            if (pos.Y <= -300)
+            {
+                if (Input.RIGHT.IsHold())
+                {
+                    pos = new Vec2f(0, -300);
+                }
+                else if (Input.LEFT.IsHold())
+                {
+                    pos = new Vec2f(0, -300);
+                }
+
             }
         }
        
