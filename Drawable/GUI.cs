818cc6e5bfddb1f16642ef0749020ed4d15b11e4
SagasaWorkPC <03220322a@gmail.com>
2019-12-16 16:15:49 +0900

    GUIのベースを追加

---
+++ Drawable/GUI.cs
@@ -0,0 +1,17 @@
+﻿using Giraffe;
+
+namespace Test.Drawable
+{
+    //画面に表示するインターフェースの親クラス
+    public abstract class GUI
+    {
+        protected Scene _scene;
+
+        public GUI(Scene scene)
+        {
+            _scene = scene;
+        }
+        public abstract void Update();
+        public abstract void Draw();
+    }
+}
