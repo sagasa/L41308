2f3b164d848b628dd699536faa21e2b7b503088b
SagasaWorkPC <03220322a@gmail.com>
2020-01-20 10:46:46 +0900

    アニメーションクラス追加

---
+++ Drawable/PlayerAnimation.cs
@@ -0,0 +1,45 @@
+﻿using Giraffe;
+
+namespace Giraffe
+{
+    /**アニメーション*/
+    public class PlayerAnimation
+    {
+        //注入した処理
+        private readonly AnimationUpdate _update;
+        //実行時間 フレーム数
+        private readonly int _life;
+        //アップデート毎の経過量 
+        private readonly float _delta;
+
+        //0からインクリメント
+        private float _time;
+        //実行対象
+        private PlayerRender _render;
+
+        public PlayerAnimation(AnimationUpdate update, int time)
+        {
+            _update = update;
+            _life = time;
+            _delta = 1f / time;
+        }
+
+        public void Init(PlayerRender render)
+        {
+            _render = render;
+            _time = 0;
+        }
+
+        public void Update()
+        {
+            _update(_render, _time / _life, _delta);
+            _time++;
+        }
+
+        public bool IsEnd() => _life <= _time;
+
+        public delegate void AnimationUpdate(PlayerRender render,float progress, float delta);
+
+        //============ アニメーション ============
+    }
+}
