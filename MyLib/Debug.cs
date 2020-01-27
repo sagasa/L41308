3d3b14adaae4863fd434b30e00eb029010d541e8
SagasaWorkPC <03220322a@gmail.com>
2020-01-27 12:21:58 +0900

    メモリが漏れてる！！

--- MyLib/Debug.cs
+++ MyLib/Debug.cs
@@ -17,6 +17,7 @@ namespace SAGASALib
 
         public static void DrawPos(Vec2f offset,Vec2f pos,string name = "",uint color= 4294902015)
         {
+#if DEBUG
             draw.Enqueue(()=>
             {
                 offset += pos;
@@ -24,30 +25,37 @@ namespace SAGASALib
                 DX.DrawBoxAA(offset.X-1, offset.Y - 15, offset.X+1, offset.Y + 15, color, DX.TRUE);
                 DX.DrawStringToHandle((int)offset.X, (int)offset.Y + 2, name + "(" + pos.X + "," + pos.Y+")", black, Handle,white);
             });
+#endif
         }
         public static void DrawVec2(Vec2f pos, Vec2f vec, string name = "", uint color = 4294902015)
         {
+#if DEBUG
             draw.Enqueue(() => {
                 DrawUtil.DrawThickBox(pos,vec,color);
                 DrawUtil.DrawThickBox(pos + vec, vec.Rotate(MyMath.Deg2Rad * 140).Normal() * 15, color);
                 DrawUtil.DrawThickBox(pos + vec, vec.Rotate(MyMath.Deg2Rad * -140).Normal() * 15, color);
                 DX.DrawStringToHandle((int)(vec+pos).X, (int)(vec + pos).Y + 2, name + "(" + vec.X + "," + vec.Y + ")", black, Handle, white);
             });
+#endif
         }
         public static void DrawCircle(Vec2f pos,float rad , uint color = 4294902015)
         {
+#if DEBUG
             draw.Enqueue(() => {
                 DX.DrawCircleAA(pos.X,pos.Y,rad,10,color,DX.FALSE);
             });
+#endif
         }
 
         private static readonly Vec2f UP = new Vec2f(0,-30);
         public static void DrawAngle(Vec2f pos, float rad, string name = "", uint color = 4294902015)
         {
+#if DEBUG
             draw.Enqueue(() => {
                 DrawUtil.DrawThickBox(pos,UP.Rotate(rad) , color,2);
                 DX.DrawStringToHandle((int)(pos).X, (int)(pos).Y + 2, name + "(" + rad*MyMath.Rad2Deg + "° )", black, Handle, white);
             });
+#endif
         }
 
         
