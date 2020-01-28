75952b6e3ef972ede25c042f97589449231c1d8c
SAGASA_main <>
2020-01-29 03:10:56 +0900

    ナビゲーター

--- Drawable/Navigator.cs
+++ Drawable/Navigator.cs
@@ -1,7 +1,34 @@
-﻿namespace Giraffe
+﻿using System;
+using System.Collections.Generic;
+using SAGASALib;
+
+namespace Giraffe
 {
     public class Navigator
     {
-        
+        private ScenePlay _scene;
+        private readonly List<Tuple<Vec2f, int>> _list = new List<Tuple<Vec2f, int>>();
+
+        public Navigator(ScenePlay scene)
+        {
+            _scene = scene;
+        }
+
+        public void AddTarget(Vec2f vec,int image)
+        {
+            _list.Add(new Tuple<Vec2f, int>(vec,image));
+        }
+
+        public void Draw()
+        {
+            _list.ForEach(entry =>
+            {
+                //画面外なら
+                if (_scene.IsInScreen(entry.Item1))
+                {
+                    
+                }
+            });
+        }
     }
 }
