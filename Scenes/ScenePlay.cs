7963909dd9c1edad133c73f950c5352f18e12987
bando <magnificent.immortal.sage@ezweb.ne.jp>
2020-01-29 14:18:42 +0900

    Update ScenePlay.cs

--- Scenes/ScenePlay.cs
+++ Scenes/ScenePlay.cs
@@ -223,7 +223,7 @@ namespace Giraffe
             gameObjects.ForEach(obj=> player.CalcInteract(obj));
             player.Update();
             playerIcon.Update();
-            playerIcon.IconPos = (29 - player.Y) * 10.2f;
+            playerIcon.IconPos = (29 - player.Y) * 10.5f;
             gameObjects.ForEach(obj => obj.Update());
             gameObjects.RemoveAll(obj => obj.IsDead());
 
