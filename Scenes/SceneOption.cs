1f9a85a2ff6b419d4ef60ebb7343d6c5de485062
kumagai <kumagaitakuma75@gmail.com>
2020-01-29 23:18:30 +0900

    オプションの画像の変更

--- Scenes/SceneOption.cs
+++ Scenes/SceneOption.cs
@@ -12,16 +12,17 @@ namespace Giraffe
         private int cursorPosX = 0;
         private int cursorPosY = 0;
         private readonly int[] fixedPosX = new int[] { 320, 520 };
-        private readonly int[] fixedPosY = new int[] { 250, 450, 650 };
+        private readonly int[] fixedPosY = new int[] { 250, 350, 450, 550 };
+
+        private int bgmPlay = 1;
+        private int sePlay = 1;
+        private const int on = 0;
+        private const int off = 1;
 
         private const float fontScale = 0.8f;
-        private bool[] playOn = new bool[] { true, true };//描画用
         private int bg = ResourceLoader.GetGraph("title_bg.png");
         private int dark = ResourceLoader.GetGraph("option/dark25.png");
-        private int bgmImage = ResourceLoader.GetGraph("option/bgm_image.png");
-        private int seImage = ResourceLoader.GetGraph("option/se_image.png");
-        private int back = ResourceLoader.GetGraph("option/back.png");
-        private int cursor = ResourceLoader.GetGraph("image_result/cursor.png");
+        private int cursor = ResourceLoader.GetGraph("option/cursor.png");
 
         public SceneOption(Game game) : base(game)
         { }
@@ -123,11 +124,13 @@ namespace Giraffe
                             if (!Game.bgmManager.playOn && cursorPosX == fixedPosX[0])
                             {
                                 Sound.Play("decision_SE.mp3");
+                                bgmPlay = on;
                                 Game.bgmManager.playOn = true;
                             }
                             else if (Game.bgmManager.playOn && cursorPosX == fixedPosX[1])
                             {
                                 Sound.Play("decision_SE.mp3");
+                                bgmPlay = off;
                                 Game.bgmManager.playOn = false;
                                 Game.bgmManager.AllRemove();
                             }
@@ -137,42 +140,34 @@ namespace Giraffe
                             if (!Sound.playOn && cursorPosX == fixedPosX[0])
                             {
                                 Sound.DefinitelyPlay("decision_SE.mp3");
+                                sePlay = on;
                                 Sound.playOn = true;
                             }
                             else if (Sound.playOn && cursorPosX == fixedPosX[1])
+                            {
+                                sePlay = off;
                                 Sound.playOn = false;
+                            }
                         }
                     }
                 }
             }
-
-            if (playOn[0] != Game.bgmManager.playOn)
-                playOn[0] = Game.bgmManager.playOn;
-            if (playOn[1] != Sound.playOn)
-                playOn[1] = Sound.playOn;
         }
 
         public override void Draw()
         {
             DX.DrawGraph(0, 0, bg);
             DX.DrawGraph(0, 0, dark);
-            DX.DrawRotaGraph(120, fixedPosY[0], fontScale, 0, bgmImage);
-            DX.DrawRotaGraph(120, fixedPosY[1], fontScale, 0, seImage);
+
             DX.DrawRotaGraph(cursorPosX, cursorPosY, fontScale, 0, cursor);
-            DX.DrawRotaGraph(fixedPosX[0], fixedPosY[fixedPosY.Length - 1], fontScale, 0, back);
-            for (int i = 0; i < playOn.Length; i++)
-            {
-                if (playOn[i])
-                {
-                    DX.DrawRotaGraph(fixedPosX[0], fixedPosY[i], fontScale, 0, ResourceLoader.GetGraph("option/on" + 1 + ".png"));
-                    DX.DrawRotaGraph(fixedPosX[1], fixedPosY[i], fontScale, 0, ResourceLoader.GetGraph("option/off" + 2 + ".png"));
-                }
-                else if (!playOn[i])
-                {
-                    DX.DrawRotaGraph(fixedPosX[0], fixedPosY[i], fontScale, 0, ResourceLoader.GetGraph("option/on" + 2 + ".png"));
-                    DX.DrawRotaGraph(fixedPosX[1], fixedPosY[i], fontScale, 0, ResourceLoader.GetGraph("option/off" + 1 + ".png"));
-                }
-            }
+
+            DX.DrawRotaGraph(120, fixedPosY[0], fontScale, 0, ResourceLoader.GetGraph("option/bgm_image.png"));
+            DX.DrawRotaGraph(120, fixedPosY[1], fontScale, 0, ResourceLoader.GetGraph("option/se_image.png"));
+            DX.DrawRotaGraph(120, fixedPosY[2], fontScale, 0, ResourceLoader.GetGraph("option/setup.png"));
+            DX.DrawRotaGraph(fixedPosX[0], fixedPosY[fixedPosY.Length - 1], fontScale, 0, ResourceLoader.GetGraph("option/back.png"));
+
+            DX.DrawRotaGraph(fixedPosX[0], fixedPosY[0], fontScale, 0, ResourceLoader.GetGraph("option/on" + bgmPlay + ".png"));
+            DX.DrawRotaGraph(fixedPosX[1], fixedPosY[1], fontScale, 0, ResourceLoader.GetGraph("option/off" + sePlay + ".png"));
         }
 
         public override void OnExit()
