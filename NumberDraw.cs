0a28e9ed54379c20fe13e21a10b7de0f7c952011
kumagai <kumagaitakuma75@gmail.com>
2020-01-31 21:07:20 +0900

    ナンバードロークラスにタイムを追加、リザルトシーンの修正
    リザルトのスコアとタイムでナンバードロークラスを使用

--- NumberDraw.cs
+++ NumberDraw.cs
@@ -10,7 +10,7 @@ namespace Giraffe
 {
     public static class NumberDraw
     {
-        //表示するスコア, 1文字目のx座標, 1文字目のy座標, 文字の間隔, 文字の拡縮, 使う画像の数字部分より前までのファイル名, ゼロ埋めするか(省略可能), 左詰めするか(省略可能)
+        //表示するスコア, 1文字目のx座標, y座標, 文字の間隔, 文字の拡縮, 使う画像の数字部分より前までのファイル名, ゼロ埋めするか(省略可), 左詰めするか(省略可)
         public static void ScoreDraw(int score, int x, int y, int interval, float fontScale, string name, bool zeroPadding = false, bool leftPadding = true)
         {
             //桁数を調べる
@@ -19,8 +19,12 @@ namespace Giraffe
             {
                 s = s / 10;
             }
+            if (zeroPadding && digit < 4)//ゼロ埋め
+            {
+                digit = 4;
+            }
             //描画
-            for (int i = 0, leftCounter = 0, unit = (int)System.Math.Pow(10, digit - 1); i < digit; i++, unit /= 10)
+            for (int i = 0, leftCounter = 0, unit = (int)Math.Pow(10, digit - 1); i < digit; i++, unit /= 10)
             {
                 for (int j = 0; j < 10; j++)
                 {
@@ -30,7 +34,7 @@ namespace Giraffe
                         leftCounter++;
                         break;
                     }
-                    if (!leftPadding && j == score / unit % 10 && (zeroPadding || score / unit != 0))//左詰めしないとき
+                    else if (!leftPadding && j == score / unit % 10 && (zeroPadding || score / unit != 0))//左詰めしないとき
                     {
                         DX.DrawRotaGraph(x + interval * i, y, fontScale, 0, ResourceLoader.GetGraph(name + j + ".png"));
                         break;
@@ -38,6 +42,7 @@ namespace Giraffe
                 }
             }
         }
+        
         //レフトカウントにレフトカウンターの値を返す　new等の描画に使うため
         public static void ScoreDraw(int score, int x, int y, int xInterval, float fontScale, string name, ref int leftCount, bool zeroPadding = false, bool leftPadding = true)
         {
@@ -47,9 +52,13 @@ namespace Giraffe
             {
                 s = s / 10;
             }
+            if (zeroPadding && digit < 4)//ゼロ埋め
+            {
+                digit = 4;
+            }
             //描画
             int leftCounter = 0;//左詰めに使用
-            for (int i = 0, unit = (int)System.Math.Pow(10, digit - 1); i < digit; i++, unit /= 10)
+            for (int i = 0, unit = (int)Math.Pow(10, digit - 1); i < digit; i++, unit /= 10)
             {
                 for (int j = 0; j < 10; j++)
                 {
@@ -59,7 +68,7 @@ namespace Giraffe
                         leftCounter++;
                         break;
                     }
-                    if (!leftPadding && j == score / unit % 10 && (zeroPadding || score / unit != 0))//左詰めしないとき
+                    else if (!leftPadding && j == score / unit % 10 && (zeroPadding || score / unit != 0))//左詰めしないとき
                     {
                         DX.DrawRotaGraph(x + xInterval * i, y, fontScale, 0, ResourceLoader.GetGraph(name + j + ".png"));
                         break;
@@ -69,6 +78,58 @@ namespace Giraffe
             leftCount = leftCounter;//レフトカウンターを返す
         }
 
-
+        //表示するタイム, 1文字目のx座標, y座標, 文字の間隔, 文字の拡縮, 使う画像の数字部分より前までのファイル名, レフトカウンターの値を返す(コロン用), フレーム数を表示するか(省略可), ゼロ埋めするか(省略可), 左詰めするか(省略可)
+        public static void TimeDraw(int[] time, int x, int y, int interval, float fontScale, string name, ref int leftCount, bool fpsDraw = false, bool zeroPadding = false, bool leftPadding = true)
+        {
+            int leftCounter = 0;
+            for (int i = 0, digit = 2, unit = (int)Math.Pow(10, digit - 1); i < digit; i++, unit /= 10)
+            {
+                for (int j = 0; j < 10; j++)//分
+                {
+                    if (leftPadding && j == time[0] / unit % 10 && (zeroPadding || unit == 1 || time[0] / unit != 0))
+                    {
+                        DX.DrawRotaGraph(x + interval * leftCounter, y, fontScale, 0, ResourceLoader.GetGraph(name + j + ".png"));
+                        leftCounter++;
+                        break;
+                    }
+                    else if (!leftPadding && j == time[0] / unit % 10 && (zeroPadding || unit == 1 || time[0] / unit != 0))
+                    {
+                        DX.DrawRotaGraph(x + interval * i, y, fontScale, 0, ResourceLoader.GetGraph(name + j + ".png"));
+                        break;
+                    }
+                }
+                for (int j = 0; j < 10; j++)//秒
+                {
+                    if (leftPadding && j == time[1] / digit % 10)
+                    {
+                        DX.DrawRotaGraph(x + interval * (2 + leftCounter), y, fontScale, 0, ResourceLoader.GetGraph(name + j + ".png"));
+                        break;
+                    }
+                    else if (!leftPadding && j == time[1] / digit % 10)
+                    {
+                        DX.DrawRotaGraph(x + interval * (2 + i), y, fontScale, 0, ResourceLoader.GetGraph(name + j + ".png"));
+                        break;
+                    }
+                }
+                if(fpsDraw)//フレーム数
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if (leftPadding && j == time[2] / digit % 10)
+                        {
+                            DX.DrawRotaGraph(x + interval * (5 + leftCounter), y, fontScale, 0, ResourceLoader.GetGraph(name + j + ".png"));
+                            break;
+                        }
+                        else if (!leftPadding && j == time[2] / digit % 10)
+                        {
+                            DX.DrawRotaGraph(x + interval * (5 + i), y, fontScale, 0, ResourceLoader.GetGraph(name + j + ".png"));
+                            break;
+                        }
+                    }
+                }
+            }
+            leftCount = leftCounter;
+        }
+        
     }
 }
--- Scenes/SceneResult.cs
+++ Scenes/SceneResult.cs
@@ -19,19 +19,20 @@ namespace Giraffe
         private string[] ranks = new string[] { "a", "b", "c" };
         private int[] rankScore = new int[] { 1100, 700, 400 };//スコア評価用,数値は仮
         private int[] rankTime = new int[] { 60, 120, 180 };//タイム評価用
-        private int[] rankBonus = new int[] { 1000, 500, 200 };//タイムボーナス用
+        private int[] timeBonus = new int[] { 1000, 500, 200 };//タイムボーナス用
         private string timeRank = "d";
         private string scoreRank = "d";
-        
+
         private bool blinkMessage = true;//点滅表示用
         private int Counter = 0;//wait,fade,blinkのカウンター
         private const int fadeTime = 120;
         //描画用定数
         private const int frameX = 240;
         private const int frameY = 100;
-        private const int fontInterval = 30;//文字同士の幅
-        private const float fontScale1 = 0.18f;//文字の大きさ
-        private const float fontScale2 = 0.1f;//タイムボーナス用
+        private const int fontInterval1 = 30;//文字の間隔
+        private const float fontScale1 = 0.18f;//文字の拡縮
+        private const int fontInterval2 = fontInterval1 / 2;//タイムボーナス用
+        private const float fontScale2 = 0.1f;
         private const float rankImageScale = 0.4f;
         private const float rankAnimationSpeed = 0.01f;
 
@@ -76,18 +77,18 @@ namespace Giraffe
             cursorPosX = fixedPosX[1];
             playerPosX = cursorPosX;
             dummyPlayer.pos = new Vec2f(cursorPosX, cursorPosY - 85);
-            testPlayer.pos = new Vec2f(fixedPosX[0], cursorPosY - 85); 
-            　
+            testPlayer.pos = new Vec2f(fixedPosX[0], cursorPosY - 85);
+
             currentScore = Game.currentScore;
             currentTime = Game.currentTime;
-            bestScore = Game.hightScore.bestScores["stage" + _scenePlay.ResourcesName] = currentScore;
-            bestTime = Game.hightScore.bestTimes["stage" + _scenePlay.ResourcesName] = currentTime;
+            bestScore = Game.hightScore.bestScores["stage" + _scenePlay.ResourcesName];
+            bestTime = Game.hightScore.bestTimes["stage" + _scenePlay.ResourcesName];
 
             for (int i = 0; i < rankTime.Length; i++)//タイムの評価
             {
                 if (rankTime[i] >= currentTime[0] * 60 + currentTime[1])
                 {
-                    currentScore += rankBonus[i];
+                    currentScore += timeBonus[i];
                     timeRank = ranks[i];
                     break;
                 }
@@ -112,9 +113,9 @@ namespace Giraffe
                 Game.hightScore.bestTimes["stage" + _scenePlay.ResourcesName] = currentTime;
                 nameGet = true;
             }
-            #if !DEBUG
+#if !DEBUG
             SaveManager.Save(HIGHTSCORE, Game.hightScore);
-            #endif
+#endif
         }
 
         public override void Update()
@@ -147,7 +148,7 @@ namespace Giraffe
                     playerPosX -= playerMoveSpeed;
                 }
             }
-            else if(!Game.fadeAction)
+            else if (!Game.fadeAction)
             {
                 if (Input.LEFT.IsPush())
                     dummyPlayer.isDunnyRight = false;
@@ -172,7 +173,7 @@ namespace Giraffe
                 blinkMessage = true;
             else if (Counter % 60 == 40)
                 blinkMessage = false;
-            //ランクのアニメーション?に使用
+            //ランクのアニメーションに使用
             if (rankExpansionAnimation)
                 rankAnimationScale += rankAnimationSpeed;
             else if (!rankExpansionAnimation)
@@ -181,24 +182,32 @@ namespace Giraffe
                 rankExpansionAnimation = false;
             else if (rankAnimationScale < 0.75)
                 rankExpansionAnimation = true;
-
+            //名前入力
             if (!Game.fadeAction && nameGet)
             {
                 //X座標,Y座標,入力可能文字数(全角は2文字扱い),保存する場所,ESCでキャンセルできる(ようにする)か
                 DX.SetFontSize(50);//文字サイズの指定
                 //文字の色を指定
-                DX.SetKeyInputStringColor(DX.GetColor(0, 0, 0),/*入力文字列の色*/
-                                          DX.GetColor(0, 0, 0),/*ＩＭＥ非使用時のカーソルの色*/
+                DX.SetKeyInputStringColor(DX.GetColor(63, 42, 11),/*入力文字列の色*/
+                                          DX.GetColor(63, 42, 11),/*ＩＭＥ非使用時のカーソルの色*/
                                           DX.GetColor(255, 255, 255),/*ＩＭＥ使用時の入力文字列の周りの色*/
-                                          DX.GetColor(0, 0, 0),/*ＩＭＥ使用時のカーソルの色*/
-                                          DX.GetColor(0, 0, 0),/*ＩＭＥ使用時の変換文字列の下線*/
+                                          DX.GetColor(63, 42, 11),/*ＩＭＥ使用時のカーソルの色*/
+                                          DX.GetColor(63, 42, 11),/*ＩＭＥ使用時の変換文字列の下線*/
                                           DX.GetColor(255, 255, 255),/*ＩＭＥ使用時の選択対象の変換候補文字列の色*/
-                                          DX.GetColor(0, 0, 0));/*ＩＭＥ使用時の入力モード文字列の色(『全角ひらがな』等)*/
-                DX.KeyInputString(110, Screen.Height / 2-40, 10, nickname, DX.TRUE);
+                                          DX.GetColor(63, 42, 11),/*ＩＭＥ使用時の入力モード文字列の色(『全角ひらがな』等)*/
+                                          DX.GetColor(255, 255, 255),/*入力文字列の縁の色*/
+                                          DX.GetColor(255, 255, 255),/*ＩＭＥ使用時の選択対象の変換候補文字列の縁の色*/
+                                          DX.GetColor(255, 255, 255),/*ＩＭＥ使用時の入力モード文字列の縁の色*/
+                                          DX.GetColor(63, 42, 11),/*ＩＭＥ使用時の変換ウインドウの縁の色*/
+                                          DX.GetColor(255, 246, 170),/*ＩＭＥ使用時の変換ウインドウの下地の色*/
+                                          DX.GetColor(255, 255, 255),/*入力文字列の選択部分(SHIFTキーを押しながら左右キーで選択)の周りの色*/
+                                          DX.GetColor(255, 255, 255),/*入力文字列の選択部分(SHIFTキーを押しながら左右キーで選択)の色*/
+                                          DX.GetColor(255, 255, 255));/*入力文字列の選択部分(SHIFTキーを押しながら左右キーで選択)の縁の色*/
+                DX.KeyInputString(110, Screen.Height / 2 - 40, 8, nickname, DX.TRUE);
                 nameGet = false;
             }
 
-            else if(!Game.fadeAction)
+            else if (!Game.fadeAction)
             {
                 if (cursorPosX != fixedPosX[0] && Input.LEFT.IsPush())//カーソルが一番左以外の時に←が押されたら、カーソルを一つ左へ
                 {
@@ -236,7 +245,7 @@ namespace Giraffe
                     Sound.Play("decision_SE.mp3");
                     Game.fadeAction = true;
                     Game.bgmManager.currentScene = "result";
-                    Game.SetScene(new ScenePlay(Game,_scenePlay.Map,_scenePlay.ResourcesName), new Fade(fadeTime, true, true));
+                    Game.SetScene(new ScenePlay(Game, _scenePlay.Map, _scenePlay.ResourcesName), new Fade(fadeTime, true, true));
                 }
                 else if (cursorPosX == fixedPosX[1] && Input.ACTION.IsPush())
                 {
@@ -260,114 +269,48 @@ namespace Giraffe
                 DX.DrawRotaGraph(fixedPosX[0], cursorPosY, 1, 0, restart);
                 DX.DrawRotaGraph(fixedPosX[1], cursorPosY, 1, 0, back);
             }
-            
 
             //testPlayer.Draw();
 
+            int scoreLeftCounter = 0;
+            int bonusLeftCounter = 0;
+            int timeLeftCounter = 0;
             //スコア
-            int digit = 1000;
-            int leftCounter1 = 0;
-            int leftCounter2 = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    if (j == currentScore / digit % 10 && currentScore / digit != 0)//現在のスコア
-                    {
-                        DX.DrawRotaGraph(frameX + fontInterval * leftCounter1, frameY, fontScale1, 0, ResourceLoader.GetGraph("image_result/result_num_" + j + ".png"));
-                        leftCounter1++;
-                    }
-                    if (j == bestScore / digit % 10 && bestScore / digit != 0)//ハイスコア
-                    {
-                        DX.DrawRotaGraph(frameX + fontInterval * leftCounter2, frameY + 50, fontScale1, 0, ResourceLoader.GetGraph("image_result/result_num_" + j + ".png"));
-                        leftCounter2++;
-                    }
-                    if (j >= currentScore / digit % 10 && j >= bestScore / digit % 10)
-                        break;
-                }
-                digit /= 10;
-            }
-            //タイムボーナス+スコア評価
-            leftCounter2 = 0;
+            NumberDraw.ScoreDraw(bestScore, frameX, frameY + 50, fontInterval1, fontScale1, "image_result/result_num_");
+            NumberDraw.ScoreDraw(currentScore, frameX, frameY, fontInterval1, fontScale1, "image_result/result_num_", ref scoreLeftCounter);
+            //タイム
+            NumberDraw.TimeDraw(bestTime, frameX, 246, fontInterval1, fontScale1, "image_result/result_num_", ref timeLeftCounter);
+            DX.DrawRotaGraph(frameX + fontInterval1 * timeLeftCounter, 246, 0.2, 0, coron);
+            NumberDraw.TimeDraw(currentTime, frameX, 200, fontInterval1, fontScale1, "image_result/result_num_", ref timeLeftCounter);
+            DX.DrawRotaGraph(frameX + fontInterval1 * timeLeftCounter, 200, 0.2, 0, coron);
+            //評価とタイムボーナス
             for (int i = 0; i < ranks.Length; i++)
             {
                 if (timeRank == ranks[i])
-                {
+                {   //タイムボーナス
+                    NumberDraw.ScoreDraw(timeBonus[i], frameX + fontInterval2 + fontInterval1 * scoreLeftCounter, frameY + 5,
+                        fontInterval2, fontScale2, "image_result/result_num_", ref bonusLeftCounter);
+                    // ()と+
+                    DX.DrawRotaGraph(frameX + fontInterval1 * scoreLeftCounter - fontInterval1 / 3, frameY + 5, fontScale2, 0, ResourceLoader.GetGraph("image_result/brackets1.png"));
+                    DX.DrawRotaGraph(frameX + fontInterval1 * scoreLeftCounter, frameY + 5, fontScale2, 0, ResourceLoader.GetGraph("image_result/plus.png"));
+                    DX.DrawRotaGraph(frameX + fontInterval1 * scoreLeftCounter + fontInterval2 * (bonusLeftCounter + 1), frameY + 5, fontScale2, 0, ResourceLoader.GetGraph("image_result/brackets2.png"));
+                    //タイム評価
                     DX.DrawRotaGraph(70, 190, rankImageScale * rankAnimationScale, 0, ResourceLoader.GetGraph("image_result/rank_" + ranks[i] + ".png"));
-
-                    //「タイムボーナス」
-
-                    digit = 1000;
-                    for (int j = 0; j < 4; j++)
-                    {
-                        for (int k = 0; k < 10; k++)
-                        {
-                            if (k == rankBonus[i] / digit % 10 && rankBonus[i] / digit != 0)
-                            {
-                                DX.DrawRotaGraph(frameX + fontInterval / 2 + fontInterval * leftCounter1 + fontInterval / 2 * leftCounter2,
-                                                 frameY+5, fontScale2, 0, ResourceLoader.GetGraph("image_result/result_num_" + k + ".png"));
-                                leftCounter2++;
-                                break;
-                            }
-                        }
-                        digit /= 10;
-                    }
-                    DX.DrawRotaGraph(frameX + fontInterval * leftCounter1 - fontInterval / 3, frameY + 5, fontScale2, 0, ResourceLoader.GetGraph("image_result/brackets1.png"));
-                    DX.DrawRotaGraph(frameX + fontInterval * leftCounter1, frameY + 5, fontScale2, 0, ResourceLoader.GetGraph("image_result/plus.png"));
-                    DX.DrawRotaGraph(frameX + fontInterval * leftCounter1 + fontInterval / 2 * (leftCounter2 + 1), frameY + 5, fontScale2, 0, ResourceLoader.GetGraph("image_result/brackets2.png"));
-                    break;
                 }
-            }
-            if (blinkMessage && currentScore > bestScore)//スコアの「new」
-                DX.DrawRotaGraph(frameX + 65 + fontInterval * (leftCounter1 + 1) + fontInterval / 2 * leftCounter2, frameY, 1, 0, newImage);
-            //タイム
-            digit = 10;
-            leftCounter1 = 0;
-            leftCounter2 = 0;
-            for (int i = 0; i < 2; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    if (j == currentTime[0] / digit % 10 && (currentTime[0] / digit != 0 || digit == 1))//現在のタイム,分
-                    {
-                        DX.DrawRotaGraph(frameX + fontInterval * leftCounter1, 200, fontScale1, 0, ResourceLoader.GetGraph("image_result/result_num_" + j + ".png"));
-                        leftCounter1++;
-                    }
-                    if (j == bestTime[0] / digit % 10 && (bestTime[0] / digit != 0 || digit == 1))//ベストタイム,分
-                    {
-                        DX.DrawRotaGraph(frameX + fontInterval * leftCounter2, 246, fontScale1, 0, ResourceLoader.GetGraph("image_result/result_num_" + j + ".png"));
-                        leftCounter2++;
-                    }
-                    if (j >= currentTime[0] / digit % 10 && j >= bestTime[0] / digit % 10)
-                        break;
-                }
-                for (int j = 0; j < 10; j++)
-                {
-                    if (j == currentTime[1] / digit % 10)//現在のタイム,秒
-                        DX.DrawRotaGraph(frameX + fontInterval * (2 + leftCounter1), 200, fontScale1, 0, ResourceLoader.GetGraph("image_result/result_num_" + j + ".png"));
-                    if (j == bestTime[1] / digit % 10)//ベストタイム,秒
-                        DX.DrawRotaGraph(frameX + fontInterval * (2 + leftCounter2), 246, fontScale1, 0, ResourceLoader.GetGraph("image_result/result_num_" + j + ".png"));
-                    if (j >= currentTime[1] / digit % 10 && j >= bestTime[1] / digit % 10)
-                        break;
-                }
-                digit /= 10;
-            }
-            DX.DrawRotaGraph(240 + fontInterval * leftCounter1, 200, 0.2, 0, coron);//現在のタイムのコロン
-            DX.DrawRotaGraph(240 + fontInterval * leftCounter2, 246, 0.2, 0, coron);//ベストタイムのコロン
-
-            for (int i = 0; i < ranks.Length; i++)//タイム評価
-            {
-                if (scoreRank == ranks[i])
-                {
+                if (scoreRank == ranks[i])//スコア評価
                     DX.DrawRotaGraph(70, 95, rankImageScale * rankAnimationScale, 0, ResourceLoader.GetGraph("image_result/rank_" + ranks[i] + ".png"));
-                    break;
-                }
             }
-            //タイムの「new」
-            if (blinkMessage && currentTime[0] * 60 + currentTime[1] < bestTime[0] * 60 + bestTime[1])
-                DX.DrawRotaGraph(frameX + 65 + fontInterval * (leftCounter1 + 3), frameY + 98, 1, 0, newImage);
+            if (blinkMessage && currentScore > bestScore)
+            {//スコアの「new」
+                DX.DrawRotaGraph(frameX + 65 + fontInterval1 * (scoreLeftCounter + 1) + fontInterval1 / 2 * bonusLeftCounter, frameY, 1, 0, newImage);
+            }
+            if (blinkMessage && (currentTime[0] * 3600 + currentTime[1] * 60 + currentTime[2] < bestTime[0] * 3600 + bestTime[1] * 60 + bestTime[2]))
+            {//タイムの「new」
+                DX.DrawRotaGraph(frameX + 65 + fontInterval1 * (timeLeftCounter + 3), 200, 1, 0, newImage);
+            }
 
-            if(nameGet)
+
+            if (!Game.fadeAction && nameGet)
             {
                 DX.DrawGraph(0, 0, ResourceLoader.GetGraph("image_result/shadow25.png"));
                 DX.DrawRotaGraph(Screen.Width / 2, Screen.Height / 2, 1, 0, ResourceLoader.GetGraph("image_result/name_space.png"));
